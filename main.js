b0a80c23563f68402578d0dd7a7ad772025e253a
young-sang <68852430+young-sang@users.noreply.github.com>
2025-05-12 20:10:07 +0900

    로컬스토리지 Read 추가

--- main.js
+++ main.js
@@ -4,43 +4,37 @@ const addList = document.getElementById("addList");
 const addBtn = document.getElementById("addBtn");
 
 const updateList = document.getElementById("updateList");
-const updateBtn = document.getElementById("updateBtn");
+const updateSubmitBtn = document.getElementById("updateSubmitBtn");
 const updateId = document.getElementById("updateId");
 
 const modal = document.getElementById("modal");
 
-num = 0;
 
-// create
-
-addBtn.addEventListener("click", () => {
-
-    if(addList.value == ''){
-        return;
-    }
-
+// makeList
+const make = (data) => {
     const tr = document.createElement("tr");
     const tdName = document.createElement("td");
     const tdId = document.createElement("td");
     const tdCheckBox = document.createElement("td");
     const tdUpdateBtn = document.createElement("td");
     const tdDeleteBtn = document.createElement("td");
-    num += 1
-    tdId.innerHTML = `<input type="hidden" value=${num} class="id"/>`;
-    tdName.innerText = addList.value;
+    const tdDate = document.createElement("td");
+
+    tdId.innerHTML = `<input type="hidden" value=${data.id} class="id"/>`;
+    tdName.innerText = data.value;
     tdName.className = "listName"
-    tdCheckBox.innerHTML = '<input type="checkbox"/>';
-    tdUpdateBtn.innerHTML = '<input type="button" class="update" />';
-    tdDeleteBtn.innerHTML = '<input type="button" class="delete" />';
+    tdCheckBox.innerHTML = `<input type="checkbox" value=${data.done}/>`;
+    tdUpdateBtn.innerHTML = '<input type="button" class="update"  value="수정"/>';
+    tdDeleteBtn.innerHTML = '<input type="button" class="delete" value="삭제" />';
+    tdDate.innerText = data.date;
 
     tr.appendChild(tdId);
     tr.appendChild(tdName);
     tr.appendChild(tdCheckBox);
     tr.appendChild(tdUpdateBtn);
     tr.appendChild(tdDeleteBtn);
+    tr.appendChild(tdDate);
     todoBox.appendChild(tr);
-
-    addList.value = "";
 
 
     // UPDATE
@@ -60,10 +54,44 @@ addBtn.addEventListener("click", () => {
 
         tr.remove();
     })
+}
 
-})
+//Read
+let list = JSON.parse(localStorage.getItem("list"));
+console.log(list)
+if(list == null){
+    list = []  
+}else{
+    for(let i = 0; i < list.length; i++){
+        make(list[i])
+    }
+}
 
-updateBtn.addEventListener("click", () => {
+
+
+
+
+//create
+addBtn.addEventListener("click", () => {
+    if(addList.value == ''){
+        return;
+    };
+    let date = new Date()
+    let data = {
+        'id' : list == null ? 1 : list.length + 1,
+        'value' : addList.value,
+        'done' : false,
+        'date' : date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate()
+    }
+    make(data);
+    addList.value = "";
+    list.push(data)
+    localStorage.setItem("list", JSON.stringify(list))
+});
+
+
+//수정 버튼 제출
+updateSubmitBtn.addEventListener("click", () => {
     const tr = document.querySelectorAll('.id');
     console.log(tr.length);
     for(i = 0; i < tr.length; i++ ){
